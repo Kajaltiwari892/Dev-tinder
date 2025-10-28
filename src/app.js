2367e202c52bbca8c87d792154f0d07c0e74f080
Kajaltiwari892 <kanjal55567@gmail.com>
2025-10-28 17:27:33 +0530

    routing done

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const express =  require('express');
 
 const app = express();
 
-// app.use  will run everytime when a request is made to the server , and if i write  any path here then it will run only for that path.
-app.use("/",(req,res)=>{
- res.send('This is the HOME page')
-}); 
+app.use('/',(req,res)=>{
+  res.end("hehhehe")
+})
 
-app.use("/hi", (req, res) => {
-  res.send("hjsheddddjhesjdhf");
-}); 
+// this will only handle GET call to /user
+app.get("/user" , (req,res)=>{
+  res.send({firstNAme:"KAjal"})
+})
 
+app.post('/user',(req,res)=>{
+  console.log("Save data to the databsee")
+  res.send("Data successfully saved to the database")
+});
+
+app.delete('/user',(req,res)=>{
+  
+  res.send("Deleted successfully")
+})
 
 app.listen(3000 , ()=>{
   console.log("http://localhost:3000/");
-})
+});
